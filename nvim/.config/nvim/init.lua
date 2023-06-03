b87240cff4d628cd9fbde1729d0f4f23a2c8525c
Łukasz Karasinski <lukasz.karasinski.15@gmail.com>
2023-06-03 09:07:57 +0000

    feat(nvim): add mason and lspconfig
    add mason, lspconfig and configure code autoformatting

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -16,3 +16,40 @@ vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappin
 
 require("lazy").setup("plugins")
 require("keybindings")
+
+-- Enable the following language servers
+--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+--
+--  Add any additional override configuration in the following tables. They will be passed to
+--  the `settings` field of the server config. You must look up that documentation yourself.
+local servers = {
+	-- clangd = {},
+	-- gopls = {},
+	-- pyright = {},
+	-- rust_analyzer = {},
+	-- tsserver = {},
+
+	lua_ls = {
+		Lua = {
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+		},
+	},
+}
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+	ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+	function(server_name)
+		require('lspconfig')[server_name].setup {
+			capabilities = capabilities,
+			on_attach = on_attach,
+			settings = servers[server_name],
+		}
+	end,
+}
---
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,12 @@
+return {
+	'neovim/nvim-lspconfig',
+	dependencies = {
+		-- Automatically install LSPs to stdpath for neovim
+		{ 'williamboman/mason.nvim', config = true },
+		'williamboman/mason-lspconfig.nvim',
+
+		-- Useful status updates for LSP
+		-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+		{ 'j-hui/fidget.nvim',       opts = {} },
+	},
+}
