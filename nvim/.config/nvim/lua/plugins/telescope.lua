d86e9dc11aa1be5bb0831778abdd1511779e5de7
Łukasz Karasinski <lukasz.karasinski.15@gmail.com>
2023-06-03 08:55:54 +0000

    feat(nvim): add telescope

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,45 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		local map = require('utils').map
+		local builtin = require('telescope.builtin')
+		local actions = require("telescope.actions")
+
+		require("telescope").setup{
+		  defaults = {
+		    mappings = {
+		      i = {
+			["<esc>"] = actions.close
+		      },
+		    },
+		  }
+		}
+
+
+		-- Map <leader>ff to Telescope's find_files
+		map('n', '<leader>ff', function()
+		    builtin.find_files({})
+		end)
+
+		-- Map <leader>fg to Telescope's live_grep
+		map('n', '<leader>fw', function()
+		    builtin.live_grep({})
+		end)
+
+		-- Map <leader>fb to Telescope's buffers
+		map('n', '<leader>fb', function()
+		    builtin.buffers({})
+		end)
+
+		-- Map <leader>fh to Telescope's help_tags
+		map('n', '<leader>fh', function()
+		    builtin.help_tags({})
+		end)
+
+		-- Map <leader>fh to Telescope's help_tags
+		map('n', '<leader>fa', function()
+		    builtin.find_files({hidden = true})
+		end)
+	end
+}
--- nvim/.config/nvim/lua/utils.lua
+++ nvim/.config/nvim/lua/utils.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.map(mode, lhs, rhs, opts)
 	opts = opts or {}
-	vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+	vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 return M
