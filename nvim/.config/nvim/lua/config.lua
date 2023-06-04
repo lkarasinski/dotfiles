36367a48597e79a7f9c1b3694f9bc1cb5a6c2dc5
Łukasz Karasinski <lukasz.karasinski.15@gmail.com>
2023-06-04 11:43:43 +0200

    feat(nvim): add presistent undos

--- nvim/.config/nvim/lua/config.lua
+++ nvim/.config/nvim/lua/config.lua
@@ -4,3 +4,10 @@ vim.opt.scrolloff = 8
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
+
+-- Create the undo directory if it doesn't exist
+vim.fn.mkdir(vim.fn.expand(os.getenv("HOME") .. "/.nvim/undo"), "p")
+
+-- Enable persistent undo
+vim.o.undofile = true
+vim.o.undodir = os.getenv("HOME") .. "/.nvim/undo"
