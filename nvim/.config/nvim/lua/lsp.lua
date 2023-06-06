055e9edf0c8de511a8c60402bcb1dc956ee9b035
Łukasz Karasinski <lukasz.karasinski.15@gmail.com>
2023-06-06 15:41:07 +0200

    feat(nvim): add lsp functionality

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -23,7 +23,21 @@ local mason_lspconfig = require 'mason-lspconfig'
 
 mason_lspconfig.setup {
 	ensure_installed = vim.tbl_keys(servers),
+
 }
+
+local on_attach = function(_, bufnr)
+	-- Create a command `:Format` local to the LSP buffer
+	vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+		vim.lsp.buf.format()
+	end, { desc = 'Format current buffer with LSP' })
+end
+
+
+
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 mason_lspconfig.setup_handlers {
 	function(server_name)
